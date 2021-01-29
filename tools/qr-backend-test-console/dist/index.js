b5b3a8df50c9e5072910be7fb9e326a409e4b35b
Matthew Nakayama <matthewnakayama@gmail.com>
2021-01-28 21:21:51 -0800

    added node projects for trying out

---
+++ tools/qr-backend-test-console/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Add the Firebase services that you want to use
+require("firebase/auth");
+require("firebase/firestore");
---
+++ tools/qr-backend-test-console/src/index.ts
@@ -0,0 +1,18 @@
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+var firebaseConfig = {
+    apiKey: "API_KEY",
+    authDomain: "PROJECT_ID.firebaseapp.com",
+    databaseURL: "https://PROJECT_ID.firebaseio.com",
+    projectId: "PROJECT_ID",
+    storageBucket: "PROJECT_ID.appspot.com",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID",
+    measurementId: "G-MEASUREMENT_ID",
+  };

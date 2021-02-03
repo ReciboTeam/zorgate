cfffdc39eaafcc584abdda710943a8d6b963ab64
Matthew Nakayama <matthewnakayama@gmail.com>
2021-02-02 20:51:55 -0800

    removed qr code console test and duplicate schema

--- tools/qr-backend-test-console/dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Add the Firebase services that you want to use
-require("firebase/auth");
-require("firebase/firestore");
